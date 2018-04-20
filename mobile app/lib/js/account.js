3a48bd5e8d32669b61c724d723d9641b2de9636b
sanr4-15 <32267189+sanr4-15@users.noreply.github.com>
2018-04-20 15:37:22 +0100

    2nd
    added backendless api, changed jquery version to compatible one and added a javascript file for account

---
+++ mobile app/lib/js/account.js
@@ -0,0 +1,15 @@
+Backendless.initApp( DA6C674F-F844-5278-FFCD-E5095DDDE000, 0AB99E65-B7F1-3285-FF2F-3EC6292B9F00 )
+
+
+$(document).on("pagecreate","#signup",function(){
+  $('#btn-submit').on("click", function(){
+    console.log("signing up user");
+      
+      var username = $('#username').val();
+      
+      alert(username);
+      
+  });             
+});       
+
+           
