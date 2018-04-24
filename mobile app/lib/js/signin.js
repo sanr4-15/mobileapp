8165cb22c21343704ea01630b1e685608ff5da27
sanr4-15 <32267189+sanr4-15@users.noreply.github.com>
2018-04-24 15:57:55 +0100

    signin
    added id to page and users can now login

--- mobile app/lib/js/signin.js
+++ mobile app/lib/js/signin.js
@@ -3,18 +3,13 @@ var API_KEY = '0AB99E65-B7F1-3285-FF2F-3EC6292B9F00';
 
 Backendless.initApp(APP_ID, API_KEY);
 
-
-
-
-
-$(document).on("pagecreate", "#signin", function () {
+$(document).on("pagecreate", "#signin", function () { console.log("pagecreate");
     $('#signinbtn').on("click", function () {
-        console.log("signing in user");
         
-        var user = new Backendless.login();
-        var pass = new Backendless.login();
-        user.username = $('#signinuser').val();
-        pass.password = $('#signinpassword').val();
+        
+        var user = $('#signinuser').val();
+        var pass = $('#signinpassword').val();
+        
         
         
         Backendless.UserService.login( user, pass, true).then(userLoggedIn).catch(gotError);
@@ -34,28 +29,3 @@ function gotError(err) // see more on error handling
     console.log("error code - " + err.statusCode);
 }
 
-
-//$(document).on("pagecreate", "#signin", function () {
-//    $('#signinbtn').on("click", function () {
-//        console.log("signing in user");
-//        
-//        var user = new Backendless.login();
-//        user.username = $('#signinuser').val();
-//        user.password = $('#signinpassword').val();
-//        
-//        Backendless.UserService.login( user, true).then(userLoggedIn).catch(gotError);
-//    });
-//});
-//
-//
-//
-//
-//function userLoggedIn(user) {
-//    console.log("user has logged in");
-//}
-//
-//function gotError(err) // see more on error handling
-//{
-//    console.log("error message - " + err.message);
-//    console.log("error code - " + err.statusCode);
-//}
