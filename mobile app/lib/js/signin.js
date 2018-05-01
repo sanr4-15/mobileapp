7d273d160b488f278e6189808fe186680df528b5
sanr4-15 <32267189+sanr4-15@users.noreply.github.com>
2018-05-01 20:55:57 +0100

    comments
    added comments for signin and signup javascript.

--- mobile app/lib/js/signin.js
+++ mobile app/lib/js/signin.js
@@ -1,17 +1,18 @@
 var APP_ID = 'DA6C674F-F844-5278-FFCD-E5095DDDE000';
 var API_KEY = '0AB99E65-B7F1-3285-FF2F-3EC6292B9F00';
-
+//initializing the server using the app id and API key
 Backendless.initApp(APP_ID, API_KEY);
 
 $(document).on("pagecreate", "#signin", function () { 
     $('#signinbtn').on("click", function () {
+//if successful, the console shows the message when the submit button is clicked
     console.log("signing in user");
         
         var user = $('#signinuser').val();
         var pass = $('#signinpassword').val();
         
         
-        
+        //logs the user in the credentials are true
         Backendless.UserService.login( user, pass, true).then(userLoggedIn).catch(gotError);
     });
 });
--- mobile app/lib/js/signup.js
+++ mobile app/lib/js/signup.js
@@ -1,13 +1,14 @@
 var APP_ID = 'DA6C674F-F844-5278-FFCD-E5095DDDE000';
 var API_KEY = '0AB99E65-B7F1-3285-FF2F-3EC6292B9F00';
-
+//initializing the server using the app id and API key
 Backendless.initApp(APP_ID, API_KEY);
 
 
 $(document).on("pagecreate", "#signup", function () {
     $('#submit').on("click", function () {
+//if successful, the console shows the message when the submit button is clicked
         console.log("signing up user");
-        
+        //creating a new user getting the values from the username, email and password input fields
         var user = new Backendless.User();
         user.username = $('#username').val();
         user.email = $('#email').val();
@@ -19,7 +20,7 @@ $(document).on("pagecreate", "#signup", function () {
 });
 
 
-
+//console shows the message if user successfully registers
 function userRegistered(user) {
     console.log("user has been registered");
 }
